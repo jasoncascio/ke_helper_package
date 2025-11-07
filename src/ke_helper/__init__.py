9b1d7bee4a0658d5a86ede0e8a7ec2cc5f5479a3
admin <admin@cascio.altostrat.com>
2025-11-07 19:15:11 +0000

    models

--- src/ke_helper/__init__.py
+++ src/ke_helper/__init__.py
@@ -1,2 +1,3 @@
 from .ke_helper import KEDatasetScanHelper, NoDDScanFoundException, get_all_scans, get_scan
 from .authentication import KEAuth
+from .models.input_models import DDTableScan, DataScan
